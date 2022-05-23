68a3e071a141403f8edfa5e03b24f76125cced15
Yerko-Contreras <yerko.contreras@uc.cl>
2022-05-22 23:01:55 -0400

    s

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,5 +111,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer._options = { host: 'https://entrega02143.herokuapp.com/' }
+  config.action_mailer._options = { host: 'https://pruebayerko.herokuapp.com/' }
 end
